7222acfaee3f2c311fe653e649aec53b10016a39
raeez nazar <raeez.nazar@pearlsofttechnologies.com>
2024-07-28 22:03:12 +0530

    initialise project

---
+++ components/NewProject.jsx
@@ -0,0 +1,17 @@
+import Input from "./Input"
+export default function NewProject() {
+    return <div>
+        <menu>
+            <li><button>Cancel</button></li>
+            <li><button>Save</button></li>
+        </menu>
+        <div>
+            <p>
+                <Input label="Title" />
+                <Input label="Description" />\
+                <Input label="Due Date" />
+            </p>
+
+        </div>
+    </div>
+}
---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import ProjectsSideBar from '../components/ProjectsSideBar'
+import NewProject from '../components/NewProject'
+function App() {
+  return (
+    <>
+      <main className='h-screen my-8 flex gap-8'>
+        <ProjectsSideBar />
+        {/* <NewProject /> */}
+
+      </main>
+
+    </>
+  );
+}
+
+export default App;
