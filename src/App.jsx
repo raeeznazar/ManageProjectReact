2617a6785d6c87aea3ff69c7739db98b16132cbc
raeez nazar <raeez.nazar@pearlsofttechnologies.com>
2024-08-01 23:39:20 +0530

    noproject

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,33 @@
 import ProjectsSideBar from './components/ProjectsSideBar'
+import NoProjectSelected from './components/NoProjectSelected';
+import { useState } from 'react';
 import NewProject from './components/NewProject';
 function App() {
+  const [projectsState, setprojectsState] = useState({
+    selectedProjectId: undefined,
+    project: []
+  })
+
+  function handleStartAddProject() {
+    setprojectsState(prevState => {
+      return {
+        ...prevState,
+        selectedProjectId: null,
+      }
+    })
+  }
+
+  let content;
+  if (projectsState.selectedProjectId === null) {
+    content = <NewProject />
+  } else if (projectsState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject} />;
+  }
   return (
     <>
       <main className='h-screen my-8 flex gap-8'>
-        <ProjectsSideBar />
-        <NewProject />
-
+        <ProjectsSideBar onStartAddProject={handleStartAddProject} />
+        {content}
       </main>
 
     </>
