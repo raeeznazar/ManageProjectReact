48a4c57188b5776e4b3811bea4a128237129aed5
raeez nazar <raeez.nazar@pearlsofttechnologies.com>
2024-08-02 22:57:35 +0530

    input save function

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,10 +1,13 @@
-export default function Input({ Label, textarea, ...props }) {
+import { forwardRef } from "react"
+
+export const Input = forwardRef(function Input({ Label, textarea, ...props }, ref) {
     return (
         <>
             <p className="flex flex-col gap-1 my-4">
                 <label className="text-sm font-bold uppercase text-stone-500" >{Label}</label>
-                {textarea ? <textarea className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"  {...props} /> : <input className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" {...props} />}
+                {textarea ? <textarea ref={ref} className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600"  {...props} /> : <input ref={ref} className="w-full p-1 border-b-2 rounded-sm border-stone-300 bg-stone-200 text-stone-600 focus:outline-none focus:border-stone-600" {...props} />}
             </p>
         </>
     )
-}
+})
+export default Input
